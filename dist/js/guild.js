b083f4ede123e5168913aefe6c0fef577cc9647c
Bob Fornal <bob@fornal.org>
2022-04-19 20:26:09 -0400

    Functional Code

---
+++ dist/js/guild.js
@@ -0,0 +1,69 @@
+
+const minutesToRefreshPage = 30;
+
+class Guild {
+  leftElement = document.querySelector('#people tbody');
+  rightElement = document.querySelector('#guild tbody');
+
+  template;
+  minutes;
+
+  constructor(minutes) {
+    this.minutes = minutes;
+    this.init();
+  }
+
+  init = async () => {
+    this.getTemplate();
+    await this.processAll();
+    this.startTimer();
+  };
+
+  startTimer = () => {
+    setInterval(() => {
+      this.processAll();
+      console.log('reloading data');
+    }, this.minutes * 1000 * 60)
+  };
+
+  processAll = async () => {
+    const edjers = await this.getEdjers();
+    const guilds = await this.getGuilds();
+
+    this.generateRows(edjers, this.leftElement);
+    this.generateRows(guilds, this.rightElement);
+  };
+
+  getEdjers = async () => {
+    const response = await fetch('./json/edjers.json');
+    return response.json();
+  };
+
+  getGuilds = async () => {
+    const response = await fetch('./json/guilds.json');
+    return response.json();
+  };
+
+  getTemplate = () => {
+    this.template = document.querySelector('#row');
+  };
+
+  generateRows = (type, element) => {
+    element.innerHTML = '';
+    type.forEach((item, index) => {
+      const position = index + 1;
+
+      const clone = this.template.content.cloneNode(true);
+      const tds = clone.querySelectorAll('td');
+
+      tds[0].textContent = position;
+      tds[1].textContent = item.name;
+      tds[2].textContent = item.xp;
+
+      element.appendChild(clone);
+    });
+  };
+
+}
+
+const guildData = new Guild(minutesToRefreshPage);
